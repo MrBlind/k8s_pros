2933c590a0cdf904042e3903565f75c65a56afe2
MrBlind <2984067240@qq.com>
2024-01-06 21:57:28 -0500

    baiding stop lesson

--- baidingyunyuansheng_Bilibili/lesson17/kubebuilder/cmd/main.go
+++ baidingyunyuansheng_Bilibili/lesson17/kubebuilder/cmd/main.go
@@ -31,6 +31,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	kubedemov1beta1 "kubebuilder.tech/api/v1beta1"
+	"kubebuilder.tech/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -42,6 +45,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(kubedemov1beta1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -85,6 +89,19 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.AppReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "App")
+		os.Exit(1)
+	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&kubedemov1beta1.App{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "App")
+			os.Exit(1)
+		}
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
---
+++ yizhanyys/gin-client-go/go.mod
@@ -0,0 +1,5 @@
+module gin-client-go
+
+go 1.20
+
+require k8s.io/client-go v0.29.0 // indirect
